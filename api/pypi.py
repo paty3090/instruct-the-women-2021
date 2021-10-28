86c18d11d5237db52e4926e90a5254450f90b984
Patrícia Alcântara <alcantarapatricia30@gmail.com>
2021-10-28 12:28:26 +0000

    heroku

--- api/pypi.py
+++ api/pypi.py
@@ -1,11 +1,21 @@
 import requests
+r = requests.get('https://api.github.com/events')
+r = requests.post('https://httpbin.org/post', data = {'key':'value'}) 
+r = requests.put('https://httpbin.org/put', data = {'key':'value'})
+r = requests.delete('https://httpbin.org/delete')
+r = requests.head('https://httpbin.org/get')
+r = requests.options('https://httpbin.org/get')
 
+payload = {'key1': 'value1', 'key2': 'value2'}
+r = requests.get('https://httpbin.org/get', params=payload)
+print(r.url)
 # Referências sobre o uso do requests:
 #
 # Fazendo requisições:
 # https://docs.python-requests.org/en/master/user/quickstart/#make-a-request
 # Usando JSON retornado:
 # https://docs.python-requests.org/en/master/user/quickstart/#json-response-content
+
 
 def version_exists(package_name, version):
     # TODO
@@ -17,4 +27,4 @@ def latest_version(package_name):
     # TODO
     # Fazer requisição na API do PyPI para descobrir a última versão
     # de um pacote. Retornar None se o pacote não existir.
-    return "?"
+    return True
--- api/serializers.py
+++ api/serializers.py
@@ -4,20 +4,15 @@ from .models import PackageRelease, Project
 from .pypi import version_exists, latest_version
 
 
-class PackageSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.Serializer):
     class Meta:
         model = PackageRelease
         fields = ["name", "version"]
-        extra_kwargs = {"version": {"required": False}}
+        extra_kwargs = {"version": {"required": True}}
 
-    def validate(self, data):
-        # TODO
-        # Validar o pacote, checar se ele existe na versão especificada.
-        # Buscar a última versão caso ela não seja especificada pelo usuário.
-        # Subir a exceção `serializers.ValidationError()` se o pacote não
-        # for válido.
+def validate(self, data):
+        #TODO
         return data
-
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
@@ -27,11 +22,6 @@ class ProjectSerializer(serializers.ModelSerializer):
     packages = PackageSerializer(many=True)
 
     def create(self, validated_data):
-        # TODO
-        # Salvar o projeto e seus pacotes associados.
-        #
-        # Algumas referência para uso de models do Django:
-        # - https://docs.djangoproject.com/en/3.2/topics/db/models/
-        # - https://www.django-rest-framework.org/api-guide/serializers/#saving-instances
-        packages = validated_data["packages"]
-        return Project(name=validated_data["name"])
+
+
+        
